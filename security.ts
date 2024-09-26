46520dfc0e68be5c60dd2c7e9cdb10ee57b7d121
Justine Ponce <poncej@hawaii.edu>
2024-09-25 21:16:18 -1000

    final

---
+++ security.ts
@@ -0,0 +1,37 @@
+type SecurityData = {
+  truePositive: number;
+  falsePositive: number;
+  trueNegative: number;
+  falseNegative: number;
+};
+
+function analyzeSecurity(data: SecurityData[]): SecurityData {
+  return data.reduce(
+    (acc, curr) => ({
+      truePositive: acc.truePositive + curr.truePositive,
+      falsePositive: acc.falsePositive + curr.falsePositive,
+      trueNegative: acc.trueNegative + curr.trueNegative,
+      falseNegative: acc.falseNegative + curr.falseNegative,
+    }),
+    {
+      truePositive: 0, falsePositive: 0, trueNegative: 0, falseNegative: 0,
+    },
+  );
+}
+
+console.log(
+  analyzeSecurity([
+    {
+      truePositive: 1,
+      falsePositive: 4,
+      trueNegative: 8,
+      falseNegative: 2,
+    },
+    {
+      truePositive: 2,
+      falsePositive: 4,
+      trueNegative: 16,
+      falseNegative: 1,
+    },
+  ]),
+);
